33462f61110e104c9cd00c446a24db4cac1a19ab
rtkazan <126402804+rtkazan@users.noreply.github.com>
2023-03-18 11:27:36 +0300

    task21

---
+++ Task21.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите координату x1 :");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y1 :");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z1 :");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату x2 :");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y2 :");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z2 :");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double s = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+Console.WriteLine($"Расстояние между двумя точками: {Math.Round(s, 2)}");
