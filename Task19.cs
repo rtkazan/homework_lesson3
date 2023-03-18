592a7e05d944b183dc3d2ae32adbad78562a43c3
rtkazan <rtkazan@mail.ru>
2023-03-18 10:36:38 +0300

    task 19

---
+++ Task19.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string nToString = Convert.ToString(n);
+if (nToString[0] == nToString[4] && nToString[1] == nToString[3])
+{
+    Console.WriteLine("да");
+}    
+else {
+    Console.WriteLine("нет");
+    }
